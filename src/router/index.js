93184cbff80179496e9e36312659bc5c77bfb333
wimgeelen <wim.geelen@student.pxl.be>
2026-05-31 20:08:53 +0200

    sick

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import WerkplekLeren from '../views/WerkplekLeren.vue'
 import Over from '../views/Over.vue'
@@ -10,7 +10,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
   scrollBehavior() {
     return { top: 0 }
